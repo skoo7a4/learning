9e8f85a0d6cb423395a3a6d352b6cf0c170a88b6
sumit <sumit.kadam@rsllearning.com>
2021-11-08 19:14:33 +0530

    commit message: Learning git commands

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,9 @@
 public class HelloWorld {
 	public static void main(String args[]){
 		System.out.println("Hi this is Sumit Kadam !");
+		int[] a={15,5,12,9};
+		for(int i=0;i<a.length;++i) {
+			System.out.println(i);
+		}
 	}
 }
